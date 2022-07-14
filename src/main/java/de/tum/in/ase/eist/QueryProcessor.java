673f428214800385df0efe4536ea4b766359c9fb
Maximilian Mitterrutzner <maxi.mitterrutzner@gmail.com>
2022-07-14 17:08:59 +0200

    Implement largest

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -2,6 +2,10 @@ package de.tum.in.ase.eist;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class QueryProcessor {
 
@@ -12,7 +16,18 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "Maximilian Mitterrutzner";
+            return "Maximilian Mitterrutzner";
+        } else if (query.contains("largest")) {
+            String string = query.split(":")[2];
+            List<Integer> integers = Arrays.stream(string.split(",")).map(s -> Integer.valueOf(s.trim())).toList();
+
+            int max = Integer.MIN_VALUE;
+
+            for(Integer i : integers) {
+                max = Math.max(max, i);
+            }
+
+            return Integer.toString(max);
         } else { // TODO extend the programm here
             return "";
         }
--- src/test/java/de/tum/in/ase/eist/QueryProcessorTest.java
+++ src/test/java/de/tum/in/ase/eist/QueryProcessorTest.java
@@ -23,6 +23,14 @@ class QueryProcessorTest {
 	}
 
 	@Test
+	void testLargest() {
+		String actual = queryProcessor.process("87cffa10: which of the following numbers is the largest: 792, 57, 14, 468");
+		if (!actual.contains("792")) {
+			fail("The QueryProcessor does not know the largest.");
+		}
+	}
+
+	@Test
 	void isNotCaseSensitive() {
 		String actual = queryProcessor.process("shakespeare");
 		if (!actual.contains("playwright")) {
